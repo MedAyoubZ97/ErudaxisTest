88e3de31f414bfe0e6a05d235cc0076bbfd69361
MedAyoubZ97 <ayoubzitouni299@gmail.com>
2026-05-18 11:36:28 +0100

    test cases update

--- src/test/java/com/erudaxisplanificationtests/automation/step_definitions/AjoutConfigStepDefinition.java
+++ src/test/java/com/erudaxisplanificationtests/automation/step_definitions/AjoutConfigStepDefinition.java
@@ -31,62 +31,61 @@ public class AjoutConfigStepDefinition {
 	   planifPage.btnClick(PlanificationPage.addConfigBtn);
 	}
 	
-	@When("saisir nom de configuration")
-	public void saisirNomDeConfiguration() {
-	   planifPage.fillField(PlanificationPage.configNameField,"test");
+	@When("saisir nom de configuration {string}")
+	public void saisirNomDeConfiguration(String input) {
+	   planifPage.fillField(PlanificationPage.configNameField,input);
 	}
 	
-	@When("saisir duree de seance")
-	public void saisirDureeDeSeance() {
-		planifPage.fillField(PlanificationPage.durationField,"60");
+	@When("saisir duree de seance {string}")
+	public void saisirDureeDeSeance(String string) {
+		planifPage.fillField(PlanificationPage.durationField,string);
 	}
 	
-	@When("saisire heure de debut matin dans le champ heure de debut matin")
-	public void saisireHeureDeDebutMatinDansLeChampHeureDeDebutMatin() {
-		planifPage.fillField(PlanificationPage.heureDebutMatinField,"08:00");
+	@When("saisire heure de debut matin dans le champ heure de debut matin {string}")
+	public void saisireHeureDeDebutMatinDansLeChampHeureDeDebutMatin(String string) {
+		planifPage.fillField(PlanificationPage.heureDebutMatinField,string);
 
 	}
-	@When("saisire heure de fin matin dans le champ heure de fin matin")
-	public void saisireHeureDeFinMatinDansLeChampHeureDeFinMatin() {
-		planifPage.fillField(PlanificationPage.heurefinMatinField,"12:00");
+	@When("saisire heure de fin matin dans le champ heure de fin matin {string}")
+	public void saisireHeureDeFinMatinDansLeChampHeureDeFinMatin(String string) {
+		planifPage.fillField(PlanificationPage.heurefinMatinField,string);
 	}
-	@When("saisire heure de debut apres midi dans le champ heure de debut apres midi")
-	public void saisireHeureDeDebutApresMidiDansLeChampHeureDeDebutApresMidi() {
-		planifPage.fillField(PlanificationPage.heureDebutApresMidiField,"12:00");
-
+	@When("saisire heure de debut apres midi dans le champ heure de debut apres midi {string}")
+	public void saisireHeureDeDebutApresMidiDansLeChampHeureDeDebutApresMidi(String string) {
+		planifPage.fillField(PlanificationPage.heureDebutApresMidiField,string);
 	}
-	@When("saisire heure de fin apres midi dans le champ heure de fin apres midi")
-	public void saisireHeureDeFinApresMidiDansLeChampHeureDeFinApresMidi() {
+	@When("saisire heure de fin apres midi dans le champ heure de fin apres midi {string}")
+	public void saisireHeureDeFinApresMidiDansLeChampHeureDeFinApresMidi(String string) {
 		planifPage.fillField(PlanificationPage.heureFinApresMidiField,"17:00");
 
 	}
-	@When("saisire heure de debut pause dejeuner dans le champ heure de debut pause dejeuner")
-	public void saisireHeureDeDebutPauseDejeunerDansLeChampHeureDeDebutPauseDejeuner() {
-	    planifPage.fillField(PlanificationPage.heureDebutDej, "12:00");
+	@When("saisire heure de debut pause dejeuner dans le champ heure de debut pause dejeuner {string}")
+	public void saisireHeureDeDebutPauseDejeunerDansLeChampHeureDeDebutPauseDejeuner(String string) {
+	    planifPage.fillField(PlanificationPage.heureDebutDej, string);
 
 	}
-	@When("saisire heure de fin pause dejeuner dans le champ heure de fin pause dejeuner")
-	public void saisireHeureDeFinPauseDejeunerDansLeChampHeureDeFinPauseDejeuner() {
-		planifPage.fillField(PlanificationPage.heureFinDej, "13:00");
+	@When("saisire heure de fin pause dejeuner dans le champ heure de fin pause dejeuner {string}")
+	public void saisireHeureDeFinPauseDejeunerDansLeChampHeureDeFinPauseDejeuner(String string) {
+		planifPage.fillField(PlanificationPage.heureFinDej, string);
 
 	}
-	@When("saisire heure de debut pause dans le champ heure de debut pause")
-	public void saisireHeureDeDebutPauseDansLeChampHeureDeDebutPause() {
-		planifPage.fillField(PlanificationPage.debutPause1, "10:00");
+	@When("saisire heure de debut pause dans le champ heure de debut pause {string}")
+	public void saisireHeureDeDebutPauseDansLeChampHeureDeDebutPause(String string) {
+		planifPage.fillField(PlanificationPage.debutPause1, string);
 
 	}
-	@When("saisire heure de fin pause dans le champ heure de fin pause")
-	public void saisireHeureDeFinPauseDansLeChampHeureDeFinPause() {
-		planifPage.fillField(PlanificationPage.finPause1, "10:10");
+	@When("saisire heure de fin pause dans le champ heure de fin pause {string}")
+	public void saisireHeureDeFinPauseDansLeChampHeureDeFinPause(String string) {
+		planifPage.fillField(PlanificationPage.finPause1, string);
 
 	}
 	@Then("click sur le bouton ajouter")
 	public void clickSurLeBoutonAjouter() {
 		planifPage.btnClick(PlanificationPage.addNewConfigBtn);
 	}
-	@Then("verifier l ajout de configuration")
-	public void verifierLAjoutDeConfiguration() throws InterruptedException {
-		planifPage.verification(PlanificationPage.successConfigCreationMsg, "Success!");
+	@Then("verifier l ajout de configuration {string}")
+	public void verifierLAjoutDeConfiguration(String string){
+		planifPage.verification(PlanificationPage.successConfigCreationMsg, string);
 		planifPage.btnClick(PlanificationPage.confirmPopupBtn);
 	}
 
--- target/classes/features_Files/AjoutConfig.feature
+++ target/classes/features_Files/AjoutConfig.feature
@@ -9,16 +9,24 @@ Feature: ajouter une configuration
 	And acceder a la formulaire d ajout de configuration 
 	
   @ajoutConfigCasValide
-  Scenario: ajouter une configuration avec cas valide
-    When saisir nom de configuration
-    And saisir duree de seance
-    And saisire heure de debut matin dans le champ heure de debut matin
-    And saisire heure de fin matin dans le champ heure de fin matin
-    And saisire heure de debut apres midi dans le champ heure de debut apres midi
-    And saisire heure de fin apres midi dans le champ heure de fin apres midi
-    And saisire heure de debut pause dejeuner dans le champ heure de debut pause dejeuner
-    And saisire heure de fin pause dejeuner dans le champ heure de fin pause dejeuner
-    And saisire heure de debut pause dans le champ heure de debut pause
-    And saisire heure de fin pause dans le champ heure de fin pause
+  Scenario Outline: ajouter une configuration avec cas valide
+    When saisir nom de configuration <nom>
+    And saisir duree de seance <duree>
+    And saisire heure de debut matin dans le champ heure de debut matin <heureDebutMatin>
+    And saisire heure de fin matin dans le champ heure de fin matin <heureFinMatin>
+    And saisire heure de debut apres midi dans le champ heure de debut apres midi <heureDebutAprésMidi>
+    And saisire heure de fin apres midi dans le champ heure de fin apres midi <heureFinAprésMidi>
+    And saisire heure de debut pause dejeuner dans le champ heure de debut pause dejeuner <heureDebutDej>
+    And saisire heure de fin pause dejeuner dans le champ heure de fin pause dejeuner <heureFinDej>
+    And saisire heure de debut pause dans le champ heure de debut pause <heureDebutPause>
+    And saisire heure de fin pause dans le champ heure de fin pause <heureFinPause>
     Then click sur le bouton ajouter
-    And verifier l ajout de configuration
+    And verifier l ajout de configuration <msg>
+    
+    Examples: 
+      | nom    | duree | heureDebutMatin  | heureFinMatin | heureDebutAprésMidi | heureFinAprésMidi | heureDebutDej | heureFinDej | heureDebutPause | heureFinPause | msg |
+      | "test" |  "60" | "08:00" | "12:00" | "12:00" | "17:00" | "12:00" | "13:00" | "10:00" | "10:05" | "Success!" |
+      | "    " |  "60" | "08:00" | "12:00" | "12:00" | "17:00" | "12:00" | "13:00" | "10:00" | "10:05" | "Erreur!"  |
+      | "/@#+-"|  "60" | "08:00" | "12:00" | "12:00" | "17:00" | "12:00" | "13:00" | "10:00" | "10:05" | "Erreur!"  |
+      | "test" |  "29" | "08:00" | "12:00" | "12:00" | "17:00" | "12:00" | "13:00" | "10:00" | "10:05" | "Erreur!"  |
+      | "test" | "541" | "08:00" | "12:00" | "12:00" | "17:00" | "12:00" | "13:00" | "10:00" | "10:05" | "Erreur!"  |
